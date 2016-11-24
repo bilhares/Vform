b9405e4d57e1c35ed661328beeb9cc720c9f85cb
bilhares <felipe.bilhares@hotmail.com>
2016-11-24 09:58:53 -0300

    Mudanças front end
    Mudanças de acordo com o projeto padronizado

--- projetos/veFormWeb/src/app/grafico-intervalo/grafico-intervalo.component.ts
+++ projetos/veFormWeb/src/app/grafico-intervalo/grafico-intervalo.component.ts
@@ -29,7 +29,7 @@ export class GraficoIntervaloComponent implements OnInit {
   //datepicker
 
   datepickerOpts: any = {
-    startDate: new Date(2016, 5, 10),
+    startDate: new Date(1900, 5, 10),
     autoclose: true,
     todayBtn: 'linked',
     todayHighlight: true,
@@ -59,11 +59,12 @@ export class GraficoIntervaloComponent implements OnInit {
   }
   montarDataInicial(dt: Date): string {
     this.mes = dt.getMonth() + 1;
-    return dt.getDate() + "/" + this.mes + "/" + dt.getFullYear();
+    console.log(dt.getFullYear);    
+    return "0"+dt.getDate() + "/"+0+ this.mes + "/" + dt.getFullYear();
   }
   montarDataFinal(dt: Date): string {
     this.mes = dt.getMonth() + 1;
-    return dt.getDate() + "/" + this.mes + "/" + dt.getFullYear();
+    return "0"+dt.getDate() + "/" +0+ this.mes + "/" + dt.getFullYear();
   }
   //fim date piker
 
@@ -81,16 +82,48 @@ export class GraficoIntervaloComponent implements OnInit {
     console.log("frm:" + this.formulario);
   }
 
-  buscaIntervaloGrafico(graficoInicial: string, graficoFinal: string, dtInicial: Date, dtFinal: Date) {
+  mostraAgenfa(agenfaInicial: string,
+    agenfaFinal: string){
+      console.log(agenfaInicial+" "+agenfaFinal);
+      
+  }
+
+  gerenciaBusca(graficoInicial: string, graficoFinal: string, dtInicial: Date, dtFinal: Date, agenfaInicial: string,
+    agenfaFinal: string, tipoDoc: string) {  
+      
+    switch (this.tipoCriterio) {
+      case 0:
+        this.buscaIntervaloGrafico(graficoInicial, graficoFinal);
+        break;
+      case 1:
+      this.buscaIntervaloGraficoData(graficoInicial, graficoFinal, dtInicial, dtFinal);
+        break;
+      case 3:
+       console.log(agenfaInicial, agenfaFinal);
+        break;
+      case 4:
+        this.buscaIntervaloGraficoDataAgenfa(graficoInicial, graficoFinal, dtInicial, dtFinal, agenfaInicial, agenfaFinal);
+        break;
+      case 5:
+        break;
+      case 6:
+        break;
+      case 8:
+        break;
+      default:
+        break;
+    }
+  }
+  buscaIntervaloGrafico(graficoInicial: string, graficoFinal: string) {
     //this.criterios();
     console.log(this.tipoCriterio);
 
     //if (!dtInicial || !dtFinal) { return; }
-    if (dtInicial != null && dtFinal != null) {
+   /* if (dtInicial != null && dtFinal != null) {
       this.dataInicial = this.montarDataInicial(dtInicial);
       this.dataFinal = this.montarDataFinal(dtFinal);
       console.log(this.dataInicial + "--" + this.dataFinal);
-    }
+    }*/
     //if (!graficoInicial || !graficoFinal) { return; }
     if (this.isProducao == 0) {//remove o caracter do numero grafico (não necessario para produção)
       graficoInicial = graficoInicial.replace(graficoInicial[0], "");
@@ -99,14 +132,40 @@ export class GraficoIntervaloComponent implements OnInit {
       console.log(graficoFinal);
     }
     console.log("{" + graficoInicial + " " + graficoFinal + " " + this.isProducao + " " + this.tipoCriterio + "}");
-
-    if (this.tipoCriterio == 0) {
       console.log("Por intervalo");
       this._httpService.buscaIntervaloGrafico(graficoInicial, graficoFinal, this.isProducao).subscribe(formulario => this.formulario = formulario);
-    } if (this.tipoCriterio == 1) {
+     /*if (this.tipoCriterio == 1) {
       console.log("com data");
       this._httpService.buscaIntervaloData(graficoInicial, graficoFinal, this.isProducao, this.dataInicial, this.dataFinal).subscribe(formulario => this.formulario = formulario);
-    }
+    }*/
+  }
+
+  buscaIntervaloGraficoData(graficoInicial: string, graficoFinal: string, dtInicial: Date, dtFinal: Date) {
+    if (this.isProducao == 0) {//remove o caracter do numero grafico (não necessario para produção)
+      graficoInicial = graficoInicial.replace(graficoInicial[0], "");
+      graficoFinal = graficoFinal.replace(graficoFinal[0], "");
+    }  
+    this.dataInicial = this.montarDataInicial(dtInicial);
+    this.dataFinal = this.montarDataFinal(dtFinal);
+    console.log(this.dataInicial + "--" + this.dataFinal);
+    console.log("{" + graficoInicial + " " + graficoFinal + " " + this.isProducao + " " + this.tipoCriterio +" "+this.dataInicial+" "+this.dataFinal+"}");
+    this._httpService.buscaIntervaloData(graficoInicial, graficoFinal, this.isProducao, this.dataInicial, this.dataFinal).subscribe(formulario => this.formulario = formulario);
+  }
+
+  buscaIntervaloGraficoDataAgenfa(graficoInicial: string, graficoFinal: string, dtInicial: Date, dtFinal: Date, 
+  agenfaInicial:string, agenfaFinal:string) {
+
+    if (this.isProducao == 0) {//remove o caracter do numero grafico (não necessario para produção)
+      graficoInicial = graficoInicial.replace(graficoInicial[0], "");
+      graficoFinal = graficoFinal.replace(graficoFinal[0], "");
+    }  
+
+    this.dataInicial = this.montarDataInicial(dtInicial);
+    this.dataFinal = this.montarDataFinal(dtFinal);
+
+    console.log(this.dataInicial + "--" + this.dataFinal);
+    console.log("{" + graficoInicial + " " + graficoFinal + " " + this.isProducao + " " + this.tipoCriterio +" "+this.dataInicial+" "+this.dataFinal+" "+agenfaInicial+" "+agenfaFinal+"}");
+    this._httpService.buscaIntervaloDataAgenfa(graficoInicial, graficoFinal, this.isProducao, this.dataInicial, this.dataFinal, agenfaInicial, agenfaFinal).subscribe(formulario => this.formulario = formulario);
   }
   //verifica se é pesquisa em produção
   verifyCheckProd(event) {
--- projetos/veFormWeb/src/app/grafico-intervalo/intervalo-grafico.service.ts
+++ projetos/veFormWeb/src/app/grafico-intervalo/intervalo-grafico.service.ts
@@ -10,6 +10,7 @@ export class IntervaloGraficoService {
 
   private urlBuscaIntervalo = "http://localhost:8080/vForm/rest/formulario/byIntervaloGrafico";
   private urlBuscaIntervaloData = "http://localhost:8080/vForm/rest/intervaloGCriterio/byIntervaloGraficoData";
+  private urlBuscaIntervaloDataAgenfa = "http://localhost:8080/vForm/rest/intervaloGCriterio/byIntervaloGraficoDataAgenfa";
   private urlLocal = "http://localhost:8080/vForm/rest/formulario/getAll";
   constructor(private _http: Http) { }
 
@@ -33,6 +34,12 @@ export class IntervaloGraficoService {
     let headers = new Headers({ 'Content-Type': 'application/json' });
     let options = new RequestOptions({ headers: headers });
 
-    return this._http.post(this.urlBuscaIntervalo, { graficoInicial, graficoFinal, isProducao, dataInicial, dataFinal }, options).map(this.extractData);
+    return this._http.post(this.urlBuscaIntervaloData, { graficoInicial, graficoFinal, isProducao, dataInicial, dataFinal }, options).map(this.extractData);
+  }
+  buscaIntervaloDataAgenfa(graficoInicial: string, graficoFinal: string, isProducao: number, dataInicial: string, dataFinal: string, agenfaInicial:string, agenfaFinal:string): Observable<Formulario[]> {
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+
+    return this._http.post(this.urlBuscaIntervaloDataAgenfa, { graficoInicial, graficoFinal, isProducao, dataInicial, dataFinal, agenfaInicial, agenfaFinal }, options).map(this.extractData);
   }
 }
