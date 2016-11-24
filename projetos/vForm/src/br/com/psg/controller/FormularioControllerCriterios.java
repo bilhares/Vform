8a0176cb312cb2991e1e47357c5b87e06165e176
bilhares <felipe.bilhares@hotmail.com>
2016-11-24 16:01:53 -0300

    controller para criterios de intervalo grafico

--- projetos/vForm/src/br/com/psg/controller/FormularioControllerCriterios.java
+++ projetos/vForm/src/br/com/psg/controller/FormularioControllerCriterios.java
@@ -29,6 +29,28 @@ public class FormularioControllerCriterios {
 		return listaFormularios;
 	}
 	
+	public List<Formulario> consultaIntervaloGraficoAgenfa(String graficoInicial, String graficoFinal, int prod,
+			String agenfaInicial, String agenfaFinal) {
+		listaFormularios = new ArrayList<>();
+		if (prod == 1) {
+			listaFormularios = dao.buscaPorIntervaloGraficoAgenfaProd(graficoInicial, graficoFinal, agenfaInicial, agenfaFinal);		
+		} else {
+			listaFormularios = dao.buscaPorIntervaloGraficoAgenfa(graficoInicial, graficoFinal, agenfaInicial, agenfaFinal);
+		}
+		return listaFormularios;
+	}
+	
+	public List<Formulario> consultaIntervaloGraficoTipoDoc(String graficoInicial, String graficoFinal, int prod,
+			int tipoDoc) {
+		listaFormularios = new ArrayList<>();
+		if (prod == 1) {
+			listaFormularios = dao.buscaPorIntervaloGraficoTipoDocProd(graficoInicial, graficoFinal, tipoDoc);				
+		} else {
+			listaFormularios = dao.buscaPorIntervaloGraficoTipoDoc(graficoInicial, graficoFinal, tipoDoc);	
+		}
+		return listaFormularios;
+	}
+	
 	public List<Formulario> consultaIntervaloGraficoDataAgenfa(String graficoInicial, String graficoFinal, int prod,
 			String dataInicial, String dataFinal,String agenfaInicial, String agenfaFinal){
 		listaFormularios = new ArrayList<>();
@@ -36,6 +58,38 @@ public class FormularioControllerCriterios {
 			listaFormularios = this.dao.buscaPorIntervaloGraficoDataAgenfaProd(graficoInicial, graficoFinal, dataInicial, dataFinal, agenfaInicial, agenfaFinal);
 		} else {
 			listaFormularios = this.dao.buscaIntervaloGraficoDataAgenfa(graficoInicial, graficoFinal, dataInicial, dataFinal, agenfaInicial, agenfaFinal);
+		}
+		return listaFormularios;
+	}
+	
+	public List<Formulario> consultaIntervaloGraficoDataTipoDoc(String graficoInicial, String graficoFinal, int prod,
+			String dataInicial, String dataFinal,int tipoDoc){
+		listaFormularios = new ArrayList<>();
+		if (prod == 1) {
+			listaFormularios = this.dao.buscaIntervaloGraficoDataTipoDocProd(graficoInicial, graficoFinal, dataInicial, dataFinal, tipoDoc);
+		} else {
+			listaFormularios = this.dao.buscaIntervaloGraficoDataTipoDoc(graficoInicial, graficoFinal, dataInicial, dataFinal, tipoDoc);
+		}
+		return listaFormularios;
+	}
+	
+	public List<Formulario> consultaIntervaloGraficoAgenfaTipoDoc(String graficoInicial, String graficoFinal, int prod,
+			String agenfaInicial, String agenfaFinal,int tipoDoc){
+		listaFormularios = new ArrayList<>();
+		if (prod == 1) {
+			listaFormularios = this.dao.buscaIntervaloGraficoAgenfaTipoDocProd(graficoInicial, graficoFinal, agenfaInicial, agenfaFinal, tipoDoc);
+		} else {
+			listaFormularios = this.dao.buscaIntervaloGraficoAgenfaTipoDoc(graficoInicial, graficoFinal, agenfaInicial, agenfaFinal, tipoDoc);
+		}
+		return listaFormularios;
+	}
+	public List<Formulario> consultaIntervaloGraficoDataAgenfaTipoDoc(String graficoInicial, String graficoFinal, int prod,
+			String dataInicial, String dataFinal,String agenfaInicial, String agenfaFinal, int tipoDoc){
+		listaFormularios = new ArrayList<>();
+		if (prod == 1) {
+			listaFormularios = this.dao.buscaIntervaloGraficoDataAgenfaTipoDocProd(graficoInicial, graficoFinal, dataInicial, dataFinal, agenfaInicial, agenfaFinal, tipoDoc);
+		} else {
+			listaFormularios = this.dao.buscaIntervaloGraficoDataTipoDoc(graficoInicial, graficoFinal, dataInicial, dataFinal, tipoDoc);
 		}
 		return listaFormularios;
 	}
